3ad56af507ee3ad49d588b2a04a27c1d91e71b54
jensneuse <jens.neuse@gmx.de>
2018-03-25 17:39:30 +0200

    set message to 2

--- brigade.js
+++ brigade.js
@@ -2,5 +2,5 @@ const { events } = require("brigadier");
 
 events.on("push", function(e, project) {
   console.log("received push for commit " + e.commit);
-  console.log("1");
+  console.log("2");
 })
